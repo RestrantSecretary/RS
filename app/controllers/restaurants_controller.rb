400455b6e5275bd2a601054eba228f2af50e5b9d
Yoshio Nasu <yoshi.seven11@gmail.com>
2014-12-13 16:45:11 +0900

    commit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,36 +1,130 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
+  #definition
+  $dataCount  = 0
+  $nowShowCount  = 0
+  $maxShowCount = 3
+  $nowChoice = nil
+ # $price = 4000
+
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @dev = 0
-    if params['dev'] != nil then
-       @dev = 1
-    end
-
+    @restaurants = []
     if params['choice'] == nil then
-    @restaurants = Restaurant.order("rank DESC").limit(3)
+    @restaurants = Restaurant.order("rank DESC").limit($maxShowCount)
    else
-    choiceParam = params['choice'];
-    case  choiceParam
-     when 'date'
-    @restaurants = Restaurant.where(:sdate =>"1").order("rank DESC").limit(3)
-     when 'party'
-    @restaurants = Restaurant.where(:sfever =>"1").order("rank DESC").limit(3)
-    # when 'rich'
-    # when 'poor'
-     when 'conpa'
-       @restaurants = Restaurant.where(:sconpa =>"1").order("rank DESC").limit(3)
-     else
-       @restaurants = Restaurant.all
-     end
+    cond = params['choice'];
+    choiceCond cond,false
    end
   end
   # GET /restaurants/1
   # GET /restaurants/1.json
+
+  def choiceCond  cond,isNext
+    case  cond
+     when 'date'
+      if isNext != "true" then clearDataCount end
+      getDateData
+      when 'fever'
+      if isNext != "true" then clearDataCount end
+      getfeverData
+      when 'rich'
+      if isNext != "true" then clearDataCount end
+      getRichData
+      when 'poor'
+      if isNext != "true" then clearDataCount end
+      getPoorData
+      when 'conpa'
+      if isNext != "true" then clearDataCount end
+      getConpaData
+      when 'shuffle'
+      if isNext != "true" then clearDataCount end
+      getShuffleData
+      when 'next'
+       $nowShowCount = $nowShowCount +  $maxShowCount
+       choiceCond $nowChoice,true
+      else
+       @restaurants = Restaurant.all
+     end
+     if @restaurants_count  == 0 then
+     @restaurants = Restaurant.all end
+  end
+
+  def getDateData
+   @restaurants = Restaurant.where(:sdate =>"1").order("rank DESC").limit($maxShowCount)
+   showCountUp
+   $nowChoice = 'date'
+  end
+  def getfeverData
+    @restaurants = Restaurant.where(:sfever =>"1").order("rank DESC").limit($maxShowCount)
+    showCountUp
+    $nowChoice = 'fever'
+  end
+  def getRichData
+    changePrice 'up'
+    topPrice   = $price + 1000
+    underPrice = $price - 1000
+    @restaurants = Restaurant.where("price >= ?  and price <= ? ", underPrice, topPrice).order("rank DESC").limit($maxShowCount)
+    @restaurants_count = Restaurant.where("price >= ?  and price <= ? ", underPrice, topPrice).count
+    showCountUp
+    $nowChoice = 'rich'
+  end
+  def getPoorData
+    changePrice 'down'
+    topPrice   = $price + 1000
+    underPrice = $price - 1000
+    @restaurants = Restaurant.where("price >= ?  and price <= ? ", underPrice, topPrice).order("rank DESC").limit($maxShowCount)
+    @restaurants_count = Restaurant.where("price >= ?  and price <= ? ", underPrice, topPrice).count
+    showCountUp
+    $nowChoice = 'poor'
+  end
+  def getConpaData
+    @restaurants = Restaurant.where(:sconpa =>"1").order("rank DESC").limit($maxShowCount)
+    showCountUp
+    $nowChoice = 'fever'
+  end
+  def getShuffleData
+    @restaurants = Restaurant.find(Restaurant.pluck(:id).shuffle[0..$maxShowCount-1])
+    showCountUp
+    $nowChoice = 'shuffle'
+  end
+
   def show
   end
+
+  #価格帯を変更する
+def changePrice updown
+  if $price == nil
+    then $price  = 4000
+   end
+  if  updown == "up" then
+    $price =  $price + 1000
+    else
+    $price =  $price - 1000
+       if $price <= 0  then
+          $price = 1000 ;
+        end
+    end
+  end
+
+#変数初期化
+def clearDataCount
+  $dataCount  = 0
+  $nowShowCount  = 0
+end
+
+def showCountUp
+  $nowShowCount  = $nowShowCount + $maxShowCount
+end
+
+def nextData data
+  data  = data + $maxShowCount
+return data
+end
+
+
 
   # GET /restaurants/new
   def new
@@ -89,6 +183,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :rank, :price, :fjpn, :fchina, :fitarian, :fother, :sdate, :sconpa, :sfever, :sother)
+      params.require(:restaurant).permit(:name, :address, :rank, :URL, :TEL, :price, :fjpn, :fchina, :fitarian,:ffranch,:fspanish, :fother, :sdate, :sconpa, :sfever, :sother, :extraText1, :extraText2, :extraText3)
     end
 end
